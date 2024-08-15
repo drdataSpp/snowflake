c31f61d8b5c5766a9cc145a99a45bde52a98ad8c
SOORYA PRAKASH PARTHIBAN <sooryaprakml@gmail.com>
2024-08-15 20:32:52 +1200

    update - master deploy script

--- snowflake_git_integration/git_master_scripts/deploy_master_test_1.sql
+++ snowflake_git_integration/git_master_scripts/deploy_master_test_1.sql
@@ -1,4 +1,5 @@
 USE ROLE SYSADMIN;
 
-EXECUTE IMMEDIATE FROM @TEST_DB.GIT.SNOWFLAKE_REPO/snowflake_git_integration/snowflake_git_integration/tables/test_db.dbt.customer_table.sql;
-EXECUTE IMMEDIATE FROM @TEST_DB.GIT.SNOWFLAKE_REPO/branches/snowflake_git_integration/snowflake_git_integration/tables/test_dbt.dbt.call_center.sql;
+EXECUTE IMMEDIATE FROM $$@TEST_DB.GIT.SNOWFLAKE_REPO/branches/snowflake_git_integration/snowflake_git_integration/tables/test_db.dbt.customer_table.sql$$;
+
+EXECUTE IMMEDIATE FROM $$@TEST_DB.GIT.SNOWFLAKE_REPO/branches/snowflake_git_integration/snowflake_git_integration/tables/test_db.dbt.call_center.sql$$;
